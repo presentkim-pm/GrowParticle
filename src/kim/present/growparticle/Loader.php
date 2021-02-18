d018e94dbdeb9be2a53c00a8b9a9ee3b8a29b9e2
PresentKim <debe3721@gmail.com>
2021-02-18 22:18:22 +0900

    Implements plugin features

--- src/kim/present/growparticle/Loader.php
+++ src/kim/present/growparticle/Loader.php
@@ -3,12 +3,30 @@ declare(strict_types=1);
 
 namespace kim\present\growparticle;
 
+use pocketmine\event\block\BlockGrowEvent;
+use pocketmine\event\Listener;
+use pocketmine\network\mcpe\protocol\SpawnParticleEffectPacket;
 use pocketmine\plugin\PluginBase;
 
-final class Loader extends PluginBase{
+final class Loader extends PluginBase implements Listener{
     protected function onEnable() : void{
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
     protected function onDisable() : void{
+    }
+
+    /** @priority MONITOR */
+    public function onBlockGrow(BlockGrowEvent $event) : void{
+        $block = $event->getBlock();
+        $pos = $block->getPos();
+
+        $pk = new SpawnParticleEffectPacket();
+        $pk->position = $pos->add(0.5, 0, 0.5);
+        $pk->particleName = "minecraft:crop_growth_emitter";
+
+        foreach($pos->getWorld()->getViewersForPosition($pos) as $player){
+            $player->getNetworkSession()->sendDataPacket($pk);
+        }
     }
 }
