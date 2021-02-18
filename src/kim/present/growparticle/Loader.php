4a28cb6ff28301f8a5a22c8509fab675c1ae0829
PresentKim <debe3721@gmail.com>
2021-02-18 22:11:22 +0900

    Init GrowParticle plugin

---
+++ src/kim/present/growparticle/Loader.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace kim\present\growparticle;
+
+use pocketmine\plugin\PluginBase;
+
+final class Loader extends PluginBase{
+    protected function onEnable() : void{
+    }
+
+    protected function onDisable() : void{
+    }
+}
